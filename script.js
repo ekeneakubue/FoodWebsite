4e236714a48869c2d7e0d7aeb5eb14f4e11aa785
Ekene Akubue <ekeneakubue@gmail.com>
2022-05-02 16:56:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let quote = "If you believe, nothing is impossible";
+document.getElementById("demo").innerHTML = quote;
+
+let x = "42" + 7;
+let y = "42" - 7;
+document.getElementById("demo1").innerHTML = "x ="+" "+x;
+document.getElementById("demo2").innerHTML = "y ="+" "+y;
+
+let number1 = "cool";
+let number2 = "trendy and fun";
+document.getElementById("number1").innerHTML = "coding is"+" "+number1;
+document.getElementById("number2").innerHTML = "coding is"+" "+number2;
+
+//1. 
+let x2 = 31 + 7;
+// enter your guess where the '0' is on the next line
+let guessOne = 0;//WRITE ANSWER =
+console.log(x2);
+
+//2.
+let y2 = 8;
+let food = "delicious";
+// Enter your guess on the next two lines, where the 'y' and 'food' is
+console.log(y2); //WRITE ANSWER = 8
+console.log(food); //WRITE ANSWER = delicious
+
+//3.
+let y1 = (9 * 6) / 3 + 2;
+// Enter your guess on the next line, where the '0' is
+let guessTwo = 0;//WRITE ANSWER = 38
+console.log(y1);
+
+//4.
+let z = "We learn to code for " + "a better future, better life, and better society.";
+// Enter your guess inside the "" below
+// Be careful! The text has to match exactly.
+let guessThree = "";//WRITE ANSWER = We learn to code for " + "a better future, better life, and better society.
+console.log(z);
